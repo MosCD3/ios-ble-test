7879342cadff318d82141a9e4c4280dd4d6b79bc
Mostafa Gamal <moscd3@gmail.com>
2021-12-07 20:10:19 -0500

    Initial Commit

---
+++ Bluetooth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bluetooth
+//
+//  Created by Mostafa Gamal on 2021-12-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
